38a49fbbfca5b84e475e570e8f4afe51cdc72551
MicheleFamoso <michelefamoso@gmail.com>
2025-04-22 11:44:13 +0200

    Create Esperienza.tsx

---
+++ src/components/Esperienza.tsx
@@ -0,0 +1,16 @@
+import { Col, Container, Row } from "react-bootstrap"
+
+const Esperienza = function () {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h3>Esperienza</h3>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Esperienza
