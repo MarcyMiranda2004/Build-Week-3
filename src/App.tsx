280f27a78d1e7cc92ef066349e4dec04620b1f05
LucioGiocondo98 <togaluc@hotmail.com>
2025-04-22 16:22:16 +0200

    fix?

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Footer from "./components/Footer";
 import LinkedInNavbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import RightSidebar from "./components/RightSidebar";
 
 function App() {
   return (
@@ -10,6 +11,10 @@ function App() {
         <LinkedInNavbar />
       </div>
       <main>
+        <RightSidebar
+          language="Italiano"
+          publicUrl="https://linkedin.com/in/Marcello Miranda"
+        />
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Necessitatibus commodi, incidunt iure ex quos veritatis debitis natus
