094d1b4b9c5d9d781d32d417fc66b8f772ccb0c2
LucioGiocondo98 <togaluc@hotmail.com>
2025-04-23 16:00:26 +0200

    fix

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -6,30 +6,28 @@ const RightSidebar = function ({
 }: {
   onUserClick: (userId: string) => void;
 }) {
-  const language = "Italiano";
-  const publicUrl = "https://linkedin.com/in/Marcello Miranda";
-
   return (
     <Col className="d-none d-md-block mt-3">
       <Card className="mb-3 shadow-sm">
         <Card.Body className="p-3">
           <div className="d-flex justify-content-between">
             <div>
-              <div className="text-muted small fw-bold">
-                Lingua del profilo <i className="bi bi-pencil"></i>
+              <div className="text-muted small fw-bold  d-flex flex-row">
+                <p>Lingua del profilo</p>
+                <i className="bi bi-pencil "></i>
               </div>
-              <div>{language}</div>
+              <div>Italiano</div>
             </div>
           </div>
           <hr />
           <div className="d-flex justify-content-between">
             <div>
-              <div className="text-muted small fw-bold">
-                Profilo pubblico e URL
+              <div className="text-muted small fw-bold d-flex flex-row ">
+                <p>Profilo pubblico e URL</p>
                 <i className="bi bi-pencil"></i>
               </div>
-              <div className="text-truncate" style={{ maxWidth: 180 }}>
-                {publicUrl}
+              <div>
+                <p>https://linkedin.com/in/marcello-miranda</p>
               </div>
             </div>
           </div>
