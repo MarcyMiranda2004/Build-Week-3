03d9c992935e0bef781ae27c775cc7aedb65f581
Marcello Miranda <marcellomiranda2004@gmail.com>
2025-04-23 09:54:07 +0200

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,26 @@
-<<<<<<< Updated upstream
-import "./App.css"
-import Footer from "./components/Footer"
-import LinkedInNavbar from "./components/Navbar"
-import "bootstrap/dist/css/bootstrap.min.css"
-import RightSidebar from "./components/RightSidebar"
-import { Col, Container, Row } from "react-bootstrap"
-import Esperienza from "./components/EsperienzaComponents/Esperienza"
-import Formazione from "./components/EsperienzaComponents/Formazione"
-import Competenze from "./components/EsperienzaComponents/Competenze"
-import Interessi from "./components/EsperienzaComponents/Interessi"
-=======
 import "./App.css";
+import Footer from "./components/Footer";
+import LinkedInNavbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import RightSidebar from "./components/RightSidebar";
+import { Col, Container, Row } from "react-bootstrap";
+import Esperienza from "./components/EsperienzaComponents/Esperienza";
+import Formazione from "./components/EsperienzaComponents/Formazione";
+import Competenze from "./components/EsperienzaComponents/Competenze";
+import Interessi from "./components/EsperienzaComponents/Interessi";
 import Consigliati from "./components/Consigliati";
 import Analisi from "./components/Analisi";
 import Attività from "./components/Attività";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 function App() {
   return (
     <>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-      <div className=" sticky-top">
+      <div className="sticky-top">
         <LinkedInNavbar />
       </div>
       <main>
         <Container>
-          <Row className=" justify-content-center">
+          <Row className="justify-content-center">
             <Col xs="6">
               <Esperienza />
               <Formazione />
@@ -52,32 +35,13 @@ function App() {
             </Col>
           </Row>
         </Container>
+        <Consigliati />
+        <Analisi />
+        <Attività />
       </main>
       <Footer />
     </>
-  )
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-      <Consigliati />
-      <Analisi />
-      <Attività />
-    </>
   );
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
 
-export default App
+export default App;
